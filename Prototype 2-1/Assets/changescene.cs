6390497428f374a6520cf512638e7747dd693a86
Harout77 <harout@live.ca>
2020-02-12 10:46:47 -0500

    Prototype 2

---
+++ Prototype 2-1/Assets/changescene.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class changescene : MonoBehaviour
+{
+
+
+
+
+    public void newscene ()
+    {
+        SceneManager.LoadScene("marslevel");
+    }
+
+}
